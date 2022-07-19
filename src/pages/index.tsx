85595baa6342aa5da710cea4d0e42faaf25dae46
Gabriel <gabriel.ifsal@gmail.com>
2022-07-18 23:26:33 -0300

    Creating the header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
+import { Flex } from '@chakra-ui/react';
+import Link from 'next/link';
+import { Image, LinkBox } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <h1>teste</h1>
+    <Flex width='100vw' height='100vh' flexDir='column'>
+      <Flex
+        position='relative'
+        as='header'
+        maxW='1200px'
+        w='100%'
+        h='94px'
+        m='0 auto'
+        align='center'
+        p='0 20px'
+      >
+        <Image src='/logo.svg' alt='Logo' draggable='false' />
+      </Flex>
+
+      
+    </Flex>
   )
 }
