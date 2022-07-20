832d655d63af6012758246254cdc68166dd8e408
Gabriel <gabriel.ifsal@gmail.com>
2022-07-19 22:04:15 -0300

    Changing the divisor line logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,28 @@ export default function Home() {
   return (
     <Flex width='100vw' height='100vh' flexDir='column'>
       <Flex
-        position='relative'
-        as='header'
-        maxW='1200px'
         w='100%'
-        h='94px'
-        m='0 auto'
-        align='center'
-        p='0 20px'
+        position='relative'
+        _before={{ content: `''`, height: '1px', bg:'#E4E6EF', width: '100%', position: 'absolute', left: 0, bottom: 0 }}
       >
-        <Image src='/logo.svg' alt='Logo' draggable='false' />
+        <Flex
+          position='relative'
+          as='header'
+          maxW='1200px'
+          w='100%'
+          h='94px'
+          m='0 auto'
+          align='center'
+          p='0 20px'
+        >
+          <Image src='/logo.svg' alt='Logo' draggable='false' />
+        </Flex>
       </Flex>
-
+      
       <Flex
         as='main'
         flex='1'
         flexDir='column'
-        position='relative'
-        _before={{ content: `''`, height: '1px', bg:'#E4E6EF', width: '100%', position: 'absolute', left: 0, top: 0 }}
       >
       </Flex>
     </Flex>
