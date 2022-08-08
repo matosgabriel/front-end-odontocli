1d7d3c43ee23f6e864e4b19c670ae974f9f346f6
Gabriel Matos <gabriel.ifsal@gmail.com>
2022-08-07 23:04:09 -0300

    Revert "Adding favicon to app"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,10 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from '../styles/theme';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
